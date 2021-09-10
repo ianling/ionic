d946a53513120255f147d766fc06409058eac3d8
Ian Ling <ian@iancaling.com>
2021-09-10 14:11:05 -0700

    [ION-1133] Add BulkSearch method

--- searches.go
+++ searches.go
@@ -1,6 +1,7 @@
 package ionic
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/url"
@@ -62,5 +63,33 @@ func (ic *IonClient) GetSearch(query, tbs, token string) ([]SearchMatch, *respon
 		return nil, nil, fmt.Errorf("failed to unmarshal product search results: %v", err.Error())
 	}
 	return results, m, nil
+
+}
+
+// BulkSearch takes one or more query strings and a "to be searched" param, then performs a productidentifier search
+// against the Ion API, returning a map of the original query string(s) to SearchMatch objects
+func (ic *IonClient) BulkSearch(queries []string, tbs, token string) (map[string][]SearchMatch, error) {
+	params := &url.Values{}
+	params.Set("tbs", tbs)
+
+	body, err := json.Marshal(queries)
+	if err != nil {
+		return nil, fmt.Errorf("session: failed to marshal login body: %v", err.Error())
+	}
+
+	buff := bytes.NewBuffer(body)
+
+	b, err := ic.Post(searchEndpoint, token, params, *buff, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get productidentifiers search: %v", err.Error())
+	}
+
+	var results map[string][]SearchMatch
+	err = json.Unmarshal(b, &results)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal product search results: %v", err.Error())
+	}
+
+	return results, nil
 
 }
