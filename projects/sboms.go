b4180a4974d14be09cb6905de98b3e6ff881d78a
Ian Ling <ian@iancaling.com>
2021-09-20 14:18:02 -0700

    Add getSbom endpoint

--- projects/sboms.go
+++ projects/sboms.go
@@ -3,36 +3,41 @@ package projects
 import "time"
 
 const (
-    // GetSbomsEndpoint is the current endpoint for retrieving an organization's SBOMs
-    GetSbomsEndpoint = "v1/project/getSBOMs"
+	// GetSbomEndpoint is the current endpoint for retrieving an organization's SBOMs
+	GetSbomEndpoint = "v1/project/getSBOM"
+	// GetSbomsEndpoint is the current endpoint for retrieving an organization's SBOMs
+	GetSbomsEndpoint = "v1/project/getSBOMs"
 )
 
+// SourceDetails contains the original package information retrieved directly from an uploaded SBOM.
 type SourceDetails struct {
-    Name    string `json:"sbom_name"`
-    Org     string `json:"sbom_org"`
-    Version string `json:"sbom_version"`
+	Name    string `json:"sbom_name"`
+	Org     string `json:"sbom_org"`
+	Version string `json:"sbom_version"`
 }
 
+// SBOMEntry represents a single entry within an SBOM.
 type SBOMEntry struct {
-    ID             string        `json:"id"`
-    SBOMID        string        `json:"sbom_id"`
-    Confidence     float32       `json:"confidence"`
-    Name           string        `json:"name"`
-    Org            string        `json:"org"`
-    Version        string        `json:"version"`
-    IonID         string        `json:"ion_id"`
-    Selected       bool          `json:"selected"`
-    LocationInSBOM int           `json:"location_in_sbom"`
-    Source         SourceDetails `json:"source"`
+	ID             string        `json:"id"`
+	SBOMID         string        `json:"sbom_id"`
+	Confidence     float32       `json:"confidence"`
+	Name           string        `json:"name"`
+	Org            string        `json:"org"`
+	Version        string        `json:"version"`
+	IonID          string        `json:"ion_id"`
+	Selected       bool          `json:"selected"`
+	LocationInSBOM int           `json:"location_in_sbom"`
+	Source         SourceDetails `json:"source"`
 }
 
+// SBOM represents a software list containing zero or more SBOMEntry objects.
 type SBOM struct {
-    ID         string      `json:"id"`
-    SbomType   string      `json:"sbom_type"`
-    SbomStatus string      `json:"sbom_status"`
-    CreatedAt  time.Time   `json:"created_at"`
-    Entries    []SBOMEntry `json:"entries"`
-    TeamID     string      `json:"team_id"`
-    OrgID      string      `json:"org_id"`
-    RulesetID  string      `json:"ruleset_id"`
+	ID         string      `json:"id"`
+	SbomType   string      `json:"sbom_type"`
+	SbomStatus string      `json:"sbom_status"`
+	CreatedAt  time.Time   `json:"created_at"`
+	Entries    []SBOMEntry `json:"entries"`
+	TeamID     string      `json:"team_id"`
+	OrgID      string      `json:"org_id"`
+	RulesetID  string      `json:"ruleset_id"`
 }
--- sboms.go
+++ sboms.go
@@ -1,29 +1,50 @@
 package ionic
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/ion-channel/ionic/projects"
-    "net/url"
+	"encoding/json"
+	"fmt"
+	"github.com/ion-channel/ionic/projects"
+	"net/url"
 )
+
+// GetSbom takes an SBOM ID.
+// Returns the requested SBOM, or any error that occurred.
+func (ic *IonClient) GetSbom(id, token string) (projects.SBOM, error) {
+	var sbom projects.SBOM
+
+	params := &url.Values{}
+	params.Set("id", id)
+
+	b, _, err := ic.Get(projects.GetSbomEndpoint, token, params, nil, nil)
+	if err != nil {
+		return sbom, fmt.Errorf("failed to get SBOM: %s", err.Error())
+	}
+
+	err = json.Unmarshal(b, &sbom)
+	if err != nil {
+		return sbom, fmt.Errorf("failed to unmarshal SBOM: %s", err.Error())
+	}
+
+	return sbom, nil
+}
 
 // GetSboms takes an organization ID, and a status to filter on, if given.
 // Returns the organization's SBOMs, filtered on the status if given, or any error that occurred.
-func (ic *IonClient) GetSboms(orgId, status, token string) ([]projects.SBOM, error) {
-    params := &url.Values{}
-    params.Set("org_id", orgId)
-    params.Set("status", status)
+func (ic *IonClient) GetSboms(orgID, status, token string) ([]projects.SBOM, error) {
+	params := &url.Values{}
+	params.Set("org_id", orgID)
+	params.Set("status", status)
 
-    b, _, err := ic.Get(projects.GetSbomsEndpoint, token, params, nil, nil)
-    if err != nil {
-        return nil, fmt.Errorf("failed to get project: %s", err.Error())
-    }
+	b, _, err := ic.Get(projects.GetSbomsEndpoint, token, params, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get SBOMs: %s", err.Error())
+	}
 
-    var sbom []projects.SBOM
-    err = json.Unmarshal(b, &sbom)
-    if err != nil {
-        return nil, fmt.Errorf("failed to unmarshal SBOMs: %s", err.Error())
-    }
+	var sboms []projects.SBOM
+	err = json.Unmarshal(b, &sboms)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal SBOMs: %s", err.Error())
+	}
 
-    return sbom, nil
+	return sboms, nil
 }
