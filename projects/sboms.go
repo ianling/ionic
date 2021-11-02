f71246211c4ff76c2b80ff521593036cd80bb35d
Ian Ling <ian@iancaling.com>
2021-11-02 13:55:46 -0700

    [ION-1186] Add SBOMEntryType to SBOMEntry

--- projects/sboms.go
+++ projects/sboms.go
@@ -16,10 +16,26 @@ type SourceDetails struct {
 	Version string `json:"sbom_version"`
 }
 
+// SBOMEntryType represents a search result's type
+type SBOMEntryType string
+
+const (
+	// SBOMEntryTypePackage represents the Package search result type
+	SBOMEntryTypePackage SBOMEntryType = "package"
+	// SBOMEntryTypeRepo represents the Repo search result type
+	SBOMEntryTypeRepo SBOMEntryType = "repo"
+	// SBOMEntryTypeProduct represents the Product search result type
+	SBOMEntryTypeProduct SBOMEntryType = "product"
+	// SBOMEntryTypeError denotes that the search result represents an error.
+	// The error message can be found in the ErrMsg field
+	SBOMEntryTypeError SBOMEntryType = "error"
+)
+
 // SBOMEntry represents a single entry within an SBOM.
 type SBOMEntry struct {
 	ID             string        `json:"id"`
 	SBOMID         string        `json:"sbom_id"`
+	Type           string        `json:"type"`
 	Confidence     float32       `json:"confidence"`
 	Name           string        `json:"name"`
 	Org            string        `json:"org"`
