41f48b4306e50a2f3d42169b5a7f97a8b91fec01
Ian Ling <ian@iancaling.com>
2021-12-10 09:57:34 -0800

    [ION-1259] Add various counts to SBOMs

--- projects/sboms.go
+++ projects/sboms.go
@@ -9,18 +9,27 @@ const (
 	GetSbomsEndpoint = "v1/project/getSBOMs"
 )
 
+// SBOMMetadata contains various piece of metadata about a particular SBOM
+type SBOMMetadata struct {
+	EntryCount                  int `json:"entry_count"`
+	ResolvedEntryCount          int `json:"resolved_entry_count"`
+	PartiallyResolvedEntryCount int `json:"partially_resolved_entry_count"`
+	UnresolvedEntryCount        int `json:"unresolved_entry_count"`
+}
+
 // SBOM represents a software list containing zero or more SBOMEntry objects.
 type SBOM struct {
-	ID         string      `json:"id"`
-	Name       string      `json:"sbom_name"`
-	Version    string      `json:"sbom_version"`
-	Supplier   string      `json:"supplier_name"`
-	SbomStatus string      `json:"sbom_status"`
-	CreatedAt  time.Time   `json:"created_at"`
-	UpdatedAt  time.Time   `json:"updated_at"`
-	EntryCount int         `json:"entry_count"`
-	Entries    []SBOMEntry `json:"entries"`
-	TeamID     string      `json:"team_id"`
-	OrgID      string      `json:"org_id"`
-	RulesetID  string      `json:"ruleset_id"`
+	ID         string       `json:"id"`
+	Name       string       `json:"sbom_name"`
+	Version    string       `json:"sbom_version"`
+	Supplier   string       `json:"supplier_name"`
+	SbomStatus string       `json:"sbom_status"`
+	CreatedAt  time.Time    `json:"created_at"`
+	UpdatedAt  time.Time    `json:"updated_at"`
+	EntryCount int          `json:"entry_count"`
+	Metadata   SBOMMetadata `json:"metadata"`
+	Entries    []SBOMEntry  `json:"entries"`
+	TeamID     string       `json:"team_id"`
+	OrgID      string       `json:"org_id"`
+	RulesetID  string       `json:"ruleset_id"`
 }
