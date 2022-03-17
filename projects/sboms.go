d3274fd85cdc858391d9c33fb764be5f9418c9c2
Ian Ling <ian@iancaling.com>
2022-03-17 13:52:17 -0700

    [DEV-1126] Add stats structs

--- projects/sboms.go
+++ projects/sboms.go
@@ -20,20 +20,53 @@ type SBOMMetadata struct {
 // SBOM represents a software list containing zero or more SBOMEntry objects.
 type SBOM struct {
 	ID               string       `json:"id"`
-	Name             string       `json:"sbom_name"`
-	Version          string       `json:"sbom_version"`
-	Supplier         string       `json:"supplier_name"`
+	Name             string       `json:"name"`
+	Version          string       `json:"version"`
+	Supplier         string       `json:"supplier"`
 	ContactName      string       `json:"contact_name"`
 	ContactEmail     string       `json:"contact_email"`
 	MonitorFrequency string       `json:"monitor_frequency"`
-	SbomStatus       string       `json:"sbom_status"`
+	SbomStatus       string       `json:"status"`
 	CreatedAt        time.Time    `json:"created_at"`
 	UpdatedAt        time.Time    `json:"updated_at"`
 	DeletedAt        *time.Time   `json:"deleted_at"`
 	EntryCount       int          `json:"entry_count"`
 	Metadata         SBOMMetadata `json:"metadata"`
+	Stats            Stats        `json:"stats"`
 	Entries          []SBOMEntry  `json:"entries"`
 	TeamID           string       `json:"team_id"`
 	OrgID            string       `json:"org_id"`
 	RulesetID        string       `json:"ruleset_id"`
+}
+
+type Risk struct {
+	Score  int `json:"score"`
+	Scopes struct {
+		Ecosystem   int `json:"ecosystem"`
+		SupplyChain int `json:"supply_chain"`
+		Software    int `json:"software"`
+	} `json:"scopes"`
+}
+
+type Compliance struct {
+	Passing int `json:"passing"`
+	Failing int `json:"failing"`
+}
+
+type Resolution struct {
+	Resolved          int `json:"resolved"`
+	PartiallyResolved int `json:"partially_resolved"`
+	Unresolved        int `json:"unresolved"`
+}
+
+type Stats struct {
+	Risk       Risk       `json:"risk"`
+	Compliance Compliance `json:"compliance"`
+	Resolution Resolution `json:"resolution"`
+}
+
+type SoftwareInventorySummary struct {
+	ID            string `json:"id"`
+	Organization  Stats  `json:"organization"`
+	SoftwareLists []SBOM `json:"software_lists"`
 }
